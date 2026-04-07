575503161d3145a09736ad03f7afbe22181734c3
Kankana Majumder <kankana_majumder@epam.com>
2026-04-07 12:49:42 +0530

    Initial commit of Practice project

---
+++ src/test/java/org/example/LoginTest.java
@@ -0,0 +1,21 @@
+package org.example;
+
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+
+public class LoginTest {
+    @Test
+    public void testValidLogin() {
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://opensource-demo.orangehrmlive.com/");
+        driver.findElement(By.xpath("//*[@id=\"app\"]/div[1]/div/div[1]/div/div[2]/div[2]/form/div[1]/div/div[2]/input")).sendKeys("Admin");
+        driver.findElement(By.xpath("//*[@id=\"app\"]/div[1]/div/div[1]/div/div[2]/div[2]/form/div[2]/div/div[2]/input")).sendKeys("admin123");
+        driver.findElement(By.xpath("//*[@id=\"app\"]/div[1]/div/div[1]/div/div[2]/div[2]/form/div[3]/button")).click();
+        // Assert that the dashboard is displayed
+        Assert.assertTrue(driver.findElement(By.xpath("/html/body/div/div[1]/div[1]/header/div[1]/div[1]")).isDisplayed());
+        driver.quit();
+    }
+}
